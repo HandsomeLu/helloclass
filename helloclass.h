30b05643251ab58a1d8411ddfab88370fa19a33f
HandsomeLu <503682014@qq.com>
2018-06-21 15:49:20 +0800

    add helloclass1

---
+++ helloclass.h
@@ -0,0 +1,19 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+class HelloClass{
+
+public:
+	HelloClass();
+	~HelloClass();
+	void sayHello();
+};
+
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+#include "helloclass.h"
+
+
+int main()
+{
+	HelloClass obj;
+	obj.sayHello();
+return 0;
+}
